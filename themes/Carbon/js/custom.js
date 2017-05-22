304c2aa7ef897f23df9aab4f6dafc1a055c381b0
Ishikawa <ishikawa@hisato.me>
2017-05-22 19:11:49 +0900

    テーマファイルを初プッシュ

---
+++ themes/Carbon/js/custom.js
@@ -0,0 +1,13 @@
+ACMS.Ready(function() {
+  ACMS.Config.offcanvas = {
+    'fixedHeaderMark'  : '.js-offcanvas-header',
+    'openBtnMark'      : '.js-offcanvas-btn',
+    'openBtnRMark'     : '.js-offcanvas-btn-r',
+    'openBtnLMark'     : '.js-offcanvas-btn-l',
+    'closeBtnMark'     : '.js-offcanvas-close',
+    'offcanvasMark'    : '.js-offcanvas',
+    'breakpoint'       : 768,
+    'throttleTime'     : 100
+  }
+  ACMS.Config.googleCodePrettifyTheme = 'tomorrow-night'
+});
